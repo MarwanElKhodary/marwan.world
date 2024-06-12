73c880820fd12d31947b755a40d90bd356a6949b
Marwan ElKhodary <marwan.elkhodary@gmail.com>
2024-06-12 19:47:49 -0400

    style: Move fonts to evade warning message, install react

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: netlify(),
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()]
 });
