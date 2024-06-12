152e51b7c6348758ac2a7abe92464480571b069e
Marwan ElKhodary <marwan.elkhodary@gmail.com>
2024-06-11 20:33:19 -0400

    style: Add Futura fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,6 +10,15 @@ export default {
 				rock: "#2B3333",
 				grey: "#6D6D6D"
 			},
+			fontFamily: {
+				futuraBook: ['"Futura PT Book"', 'sans-serif'],
+				futuraLight: ['"Futura PT Light"', 'sans-serif'],
+				futuraMedium: ['"Futura PT Medium"', 'sans-serif'],
+				futuraDemi: ['"Futura PT Demi"', 'sans-serif'],
+				futuraHeavy: ['"Futura PT Heavy"', 'sans-serif'],
+				futuraBold: ['"Futura PT Bold"', 'sans-serif'],
+				futuraExtraBold: ['"Futura PT Extra Bold"', 'sans-serif'],
+			},
 		},
 	},
 	plugins: [require("@tailwindcss/typography")],
