e7a0132b19e4c8f372062c3f0b581486062f976a
Marwan ElKhodary <marwan.elkhodary@gmail.com>
2024-06-17 22:44:35 -0400

    style: Add underlines in navbar, add projects

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -137,7 +137,7 @@ window.applyMenuItemClasses = () => {
 	const menuItems = document.querySelectorAll("#menu a");
 	for (let i = 0; i < menuItems.length; i++) {
 		if (menuItems[i].pathname === window.location.pathname) {
-			menuItems[i].classList.add("text-neutral-900", "dark:text-white");
+			menuItems[i].classList.add("underline", "decoration-4", "underline-offset", "decoration-gasoline"); 
 		}
 	}
 	//:class="{ 'text-neutral-900 dark:text-white': window.location.pathname == '{menu.url}', 'text-neutral-700 dark:text-neutral-400': window.location.pathname != '{menu.url}' }"
