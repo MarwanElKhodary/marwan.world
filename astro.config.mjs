13d559ebf236f0465c8ed899578708d51b778484
Marwan ElKhodary <marwan.elkhodary@gmail.com>
2024-10-14 17:36:17 -0400

    feat: Make code blocks dynamic

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,16 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify";
 
-// https://astro.build/config
 export default defineConfig({
   markdown: {
     shikiConfig: {
-      theme: "dracula-soft",
-      // themes: { //need to figure out how dark mode works before adding these themes
-      //   light: 'rose-pine-dawn',
-      //   dark: "slack-dark",
-      // },
+      themes: {
+        light: 'everforest-light',
+        // light: 'rose-pine-dawn',
+        // light: 'solarized-light',
+        // light: 'vitesse-light',
+        dark: "everforest-dark",
+      },
       wrap: true,
     },
   },
